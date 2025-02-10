54a5a470ebd87c16620f67e4dfadfc23eaa548cb
FallenL3vi <seba12351212@gmail.com>
2025-02-10 23:40:08 +0100

    Costs window Add/Delete

--- src/main.py
+++ src/main.py
@@ -27,12 +27,15 @@ MONTHS = [
     "November",
     "December"]
 
-costs_dict = {}
+costs_dict = {"January": {
+    "Kompen" : 12.0,
+    "wokmm" : 1.0
+}}
 gains_dict = {}
 open_costs = {}
 open_gains = {}
 
-class CostsWindow():
+class Window():
     def __init__(self, root, month):
         self.root = root
         self.month = month
@@ -47,7 +50,7 @@ class CostsWindow():
     def open_window(self):
         self.window = Toplevel(root)
         self.window.title(f"Costs of {self.month}")
-        self.window.geometry("200x200")
+        self.window.geometry("300x200")
         self.window.protocol("W_DELETE_WINDOW", self.on_exit)
     
     def get_active_window(self):
@@ -55,11 +58,78 @@ class CostsWindow():
             return False
         return True
 
+
+class CostsWindow(Window):
+    def __init__(self, root, month):
+        self.list_box = None
+        self.cost_name = StringVar()
+        self.cost_value = StringVar()
+        super().__init__(root, month)
+    
+    def on_exit(self):
+        super().on_exit()
+        self.list_box = None
+
+    def open_window(self):
+        super().open_window()
+        #Filling costs list
+        global costs_dict
+        self.list_box = Listbox(self.window, height=5)
+        print(f"Self 1: {self}")
+        if self.month in costs_dict:
+            index = 0
+            for key in costs_dict[self.month].keys():
+                self.list_box.insert(index, str(key + " => " + str(costs_dict[self.month][key])))
+                index += 1
+        self.list_box.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.list_box.select_set(0)
+        scrollbar = Scrollbar(self.window, orient=VERTICAL, command=self.list_box.yview)
+        scrollbar.grid(column=1, row=0, sticky=(W,N,S))
+        self.list_box['yscrollcommand'] = scrollbar.set
+
+
+        delete_button = ttk.Button(self.window, text="Delete", default="normal", command=self.delete_element)
+        delete_button.grid(column=2, row=0, sticky=(W,N))
+
+        name_label = ttk.Label(self.window, text = "Name")
+        value_label = ttk.Label(self.window, text="Value")
+
+        name_label.grid(column=0, row=1, sticky=N)
+        value_label.grid(column=1, row=1, sticky=N)
+
+        name_entry = ttk.Entry(self.window, textvariable=self.cost_name)
+        value_entry = ttk.Entry(self.window, textvariable=self.cost_value)
+
+        name_entry.grid(column=0, row=2, sticky=N)
+        value_entry.grid(column=1, row=2, sticky=N)
+
+        add_button = ttk.Button(self.window, text="Add", default="normal", command=self.add_element)
+        add_button.grid(column=0, row=3, sticky=N)
+    
+    def delete_element(self, *args):
+        if self.list_box == None:
+            return
+        indxs = self.list_box.curselection()
+        for index in indxs:
+            self.list_box.delete(index, index)
+
+    def add_element(self):
+        if self.list_box == None:
+            return
+        if self.cost_name.get() != "" and self.cost_value.get() != "":
+            self.list_box.insert(self.list_box.size(), str(self.cost_name.get() + " => " + self.cost_value.get()))
+            self.cost_name.set("")
+            self.cost_value.set("")
+        else:
+            return
+
+
 def select_month(*args):
     global list_box
     idxs = list_box.curselection()
     if len(idxs)==1:
         index = int(idxs[0])
+        print(index)
         list_box.see(index)
         current_month.set(f"Selectd month: {MONTHS[index]}")
         pass
